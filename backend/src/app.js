99b32c1caac4f7a8b092547ae48f08ec86715f09
mariaspereira <maddu_pereira@outlook.com>
2025-04-23 17:39:22 -0300

    login-autentucacao-node.js

--- backend/src/app.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const app = express();
-const port = 3000; //Porta que a aplicação está rodando
-
-app.get('/', (request, response) => response.send(`Aplicação rodando na porta: ${port}`));
-
-app.listen(port);
